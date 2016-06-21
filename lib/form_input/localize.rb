3846945039982e6db741155210825b62df8d15ba
Patrik Rak <patrik@raxoft.cz>
2016-06-21 13:02:43 +0000

    Use Hash[] instead of to_h even in the localize helper to remain ruby 2.0.0 compatible.

--- lib/form_input/localize.rb
+++ lib/form_input/localize.rb
@@ -7,7 +7,7 @@ require 'form_input/r18n'
 class Hash
   # Convert all hash keys to strings recursively.
   def stringify_keys
-    map{ |k, v| [ k.to_s, v.respond_to?( :stringify_keys ) ? v.stringify_keys : v ] }.to_h
+    Hash[ map{ |k, v| [ k.to_s, v.respond_to?( :stringify_keys ) ? v.stringify_keys : v ] } ]
   end
 end
 
@@ -23,13 +23,13 @@ class FormInput
 
       # Get hash of all parameter values which may need to be localized.
       def translation_hash
-        opts.select{ |k, v| v.is_a? String }.sort_by{ |k, v| [ translation_order( k ), k ] }.to_h
+        Hash[ opts.select{ |k, v| v.is_a? String }.sort_by{ |k, v| [ translation_order( k ), k ] } ]
       end
   end
 
   # Get hash of all form values which may need to be localized.
   def self.translation_hash
-    hash = form_params.map{ |k, v| [ k, v.translation_hash ] }.reject{ |k, v| v.empty? }.to_h
+    hash = Hash[ form_params.map{ |k, v| [ k, v.translation_hash ] }.reject{ |k, v| v.empty? } ]
     hash[ :steps ] = form_steps.reject{ |k, v| v.nil? } if form_steps
     hash
   end
@@ -41,7 +41,7 @@ class FormInput
 
   # Get string containing YAML representation of the default R18n translation for all/given FormInput classes.
   def self.default_translation(forms = self.forms)
-    hash = forms.map{ |x| [ x.translation_name, x.translation_hash ] }.reject{ |k, v| v.empty? }.to_h
+    hash = Hash[ forms.map{ |x| [ x.translation_name, x.translation_hash ] }.reject{ |k, v| v.empty? } ]
     YAML::dump( { forms: hash }.stringify_keys )
   end
 end
